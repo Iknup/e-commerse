3bf787bf9e39316cbb695af4c7e319bfd869260f
Iknup <leesiyeol03@gmail.com>
2023-09-17 23:36:44 -0400

    item page v0.1

--- src/app/men/shorts/00123DE/components/detail/Size.tsx
+++ src/app/men/shorts/00123DE/components/detail/Size.tsx
@@ -2,7 +2,7 @@ import SizeBox from './SizeBox';
 
 const Size = ({ sizes }: { sizes: string[] }) => {
   const sizeBoxes = sizes.map((size, i) => {
-    return <SizeBox size={size} />;
+    return <SizeBox key={i} size={size} />;
   });
   return <div className='flex items-center flex-wrap mt-2'>{sizeBoxes}</div>;
 };
