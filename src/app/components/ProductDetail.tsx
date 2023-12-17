08e03c9214ddda828e0989875729bfe7f181b625
Iknup <leesiyeol03@gmail.com>
2023-12-16 23:32:38 -0400

    scroll top button design changed

--- src/app/components/ProductDetail.tsx
+++ src/app/components/ProductDetail.tsx
@@ -173,7 +173,7 @@ const ProductDetail = ({
             viewBox='0 0 24 24'
             strokeWidth={1.5}
             stroke='currentColor'
-            className='w-8 h-8'
+            className='w-14 h-14'
           >
             <path
               strokeLinecap='round'
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,67 +2,95 @@ import Image from 'next/image';
 import MainSlider from './components/MainSlider';
 import NewCollection from './components/NewCollection';
 import Link from 'next/link';
+import LinkBtn from './components/Animation/LinkBtn';
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen w-full flex-col items-center'>
-      <div className='mt-5'>
-        <MainSlider
-          src={['/main-man01.jpg', '/main-man02.jpg']}
-          alt='main-man'
-          ratio={{ sm: 2, md: 2 }}
-          className=''
-        />
-      </div>
-      <div className='mt-10 w-full'>
-        <h1 className='text-2xl mb-4 font-semibold text-center'>
-          Nueva Colleción
-        </h1>
-        <NewCollection />
-        <div className='text-center mt-3 font-bold '>
-          <Link href={'/'}>Ver todo</Link>
+    <>
+      <main className='sm:hidden'>
+        <div className='flex justify-center items-center bg-black w-full h-screen'>
+          <div className='text-center text-white pb-5'>
+            <h1 className='font-extrabold  text-5xl tracking-[0.25em] mb-5'>
+              SISLEY
+            </h1>
+            <p className='text-lg tracking-[0.25em]'>La moda de Santa Cruz</p>
+            <div
+              className='flex justify-evenly mt-5 tracking-widest 
+            text-xl font-bold'
+            >
+              <LinkBtn
+                className='p-3 w-[45%] rounded-lg underline underline-offset-8'
+                url={'/men'}
+                name='HOMBRE'
+              />
+              <LinkBtn
+                className='p-3 w-[45%] rounded-lg underline underline-offset-8'
+                url={'women'}
+                name='MUJER'
+              />
+            </div>
+          </div>
         </div>
-      </div>
-      <div className='mt-5 w-full grid sm:grid-cols-2'>
-        <div className='relative group'>
-          <Image
-            src={'/home_crop_01.jpg'}
-            width={1600}
-            height={1600}
-            alt='home_man'
+      </main>
+      <main className='hidden sm:flex min-h-screen w-full flex-col items-center'>
+        <div className='mt-5'>
+          <MainSlider
+            src={['/main-man01.jpg', '/main-man02.jpg']}
+            alt='main-man'
+            ratio={{ sm: 2, md: 2 }}
+            className=''
           />
-          <div
-            className='absolute top-0 w-full h-full bg-opacity-50 bg-black flex sm:hidden items-center 
+        </div>
+        <div className='mt-10 w-full'>
+          <h1 className='text-2xl mb-4 font-semibold text-center'>
+            Nueva Colleción
+          </h1>
+          <NewCollection />
+          <div className='text-center mt-3 font-bold '>
+            <Link href={'/'}>Ver todo</Link>
+          </div>
+        </div>
+        <div className='mt-5 w-full grid sm:grid-cols-2'>
+          <div className='relative group'>
+            <Image
+              src={'/home_crop_01.jpg'}
+              width={1600}
+              height={1600}
+              alt='home_man'
+            />
+            <div
+              className='absolute top-0 w-full h-full bg-opacity-50 bg-black flex sm:hidden items-center 
             justify-center 
             sm:group-hover:flex
             sm:group-hover:bg-black sm:group-hover:bg-opacity-50 text-white '
-          >
-            <Link href={'/men'} className='text-center text-xl font-bold'>
-              <p>SISLEY</p>
-              <p className='overline'>HOMBRE</p>
-            </Link>
+            >
+              <Link href={'/men'} className='text-center text-xl font-bold'>
+                <p>SISLEY</p>
+                <p className='overline'>HOMBRE</p>
+              </Link>
+            </div>
           </div>
-        </div>
-        <div className='relative group'>
-          <Image
-            src={'/home_crop_02.jpg'}
-            width={1600}
-            height={1600}
-            alt='home_woman'
-          />
-          <div
-            className='absolute top-0 w-full h-full bg-opacity-50 bg-black flex sm:hidden items-center 
+          <div className='relative group'>
+            <Image
+              src={'/home_crop_02.jpg'}
+              width={1600}
+              height={1600}
+              alt='home_woman'
+            />
+            <div
+              className='absolute top-0 w-full h-full bg-opacity-50 bg-black flex sm:hidden items-center 
             justify-center 
             sm:group-hover:flex
             sm:group-hover:bg-black sm:group-hover:bg-opacity-50 text-white '
-          >
-            <Link href={'/'} className='text-center text-xl font-bold'>
-              <p>SISLEY</p>
-              <p className='overline'>MUJER</p>
-            </Link>
+            >
+              <Link href={'/'} className='text-center text-xl font-bold'>
+                <p>SISLEY</p>
+                <p className='overline'>MUJER</p>
+              </Link>
+            </div>
           </div>
         </div>
-      </div>
-    </main>
+      </main>
+    </>
   );
 }
